91ec9f03e1ba70ce30eb759744fecdbf29779d73
Pushpak Rangaiah <pushpak@users.noreply.github.com>
2019-05-13 14:50:04 +0530

    iOS fix CORS issues

--- ios/RCTHttpServer.m
+++ ios/RCTHttpServer.m
@@ -65,6 +65,7 @@ RCT_EXPORT_METHOD(start:(NSInteger) port
         [self initResponseReceivedFor:_webServer forType:@"PUT"];
         [self initResponseReceivedFor:_webServer forType:@"GET"];
         [self initResponseReceivedFor:_webServer forType:@"DELETE"];
+        [self initResponseReceivedFor:_webServer forType:@"OPTIONS"];
         
         [_webServer startWithPort:port bonjourName:serviceName];
     });
